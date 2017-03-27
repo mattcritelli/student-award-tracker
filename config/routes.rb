152889317337bf36018ca99a01094e07f0969c99
mattcritelli <matthew.critelli@gmail.com>
2017-03-27 11:05:24 -0600

    Create user model and update user controller to allow users to be created.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   resources :courses do
     member do
       get :roll
@@ -15,5 +16,4 @@ Rails.application.routes.draw do
       post :course_remove
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
